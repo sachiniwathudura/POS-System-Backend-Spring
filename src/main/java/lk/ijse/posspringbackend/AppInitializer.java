e2a679839606fcc12f5a5fdc119c329a15c7932c
sachiniwathudura <sachiniwathudura@gmail.com>
2024-10-08 08:49:46 +0530

    create AppInitializer

---
+++ src/main/java/lk/ijse/posspringbackend/AppInitializer.java
@@ -0,0 +1,24 @@
+package lk.ijse.posspringbackend;
+
+
+import lk.ijse.posspringbackend.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
