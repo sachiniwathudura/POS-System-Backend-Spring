c826f69885f789139c10a9ef6c451c8c1c5789d2
sachiniwathudura <sachiniwathudura@gmail.com>
2024-10-08 09:21:36 +0530

    Create HealthCheckController

--- src/main/java/lk/ijse/posspringbackend/controller/HealthCheckController.java
+++ src/main/java/lk/ijse/posspringbackend/controller/HealthCheckController.java
@@ -1,4 +1,12 @@
 package lk.ijse.posspringbackend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/healthCheck")
 public class HealthCheckController {
+    public String healthCheck(){
+        return "POS SYSTEM BACKEND IS RUNNING";
+    }
 }
