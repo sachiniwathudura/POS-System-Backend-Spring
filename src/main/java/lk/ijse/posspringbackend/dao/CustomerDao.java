f610211f776170b4ce0820e8a541d95e61281258
sachiniwathudura <sachiniwathudura@gmail.com>
2024-10-15 10:22:59 +0530

    update customer dao interface

--- src/main/java/lk/ijse/posspringbackend/dao/CustomerDao.java
+++ src/main/java/lk/ijse/posspringbackend/dao/CustomerDao.java
@@ -2,7 +2,9 @@ package lk.ijse.posspringbackend.dao;
 
 import lk.ijse.posspringbackend.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerDao extends JpaRepository<Customer, String> {
     Customer getCustomerById(String id);
 }
