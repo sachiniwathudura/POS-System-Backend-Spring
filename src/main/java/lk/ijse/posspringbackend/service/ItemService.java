9bd0ffe4c5c0fe142dc5493b289c6b5baab8e026
sachiniwathudura <sachiniwathudura@gmail.com>
2024-10-15 10:19:44 +0530

    update ItemServiceImpl

--- src/main/java/lk/ijse/posspringbackend/service/ItemService.java
+++ src/main/java/lk/ijse/posspringbackend/service/ItemService.java
@@ -1,4 +1,16 @@
 package lk.ijse.posspringbackend.service;
 
+
+
+import lk.ijse.posspringbackend.customObj.ItemResponse;
+import lk.ijse.posspringbackend.dto.ItemDTO;
+
+import java.util.List;
+
 public interface ItemService {
+    void saveItem(ItemDTO itemDTO);
+    void updateItem( String code ,ItemDTO itemDTO);
+    void deleteItem(String code );
+    ItemResponse getSelectedItem(String code);
+    List<ItemDTO> getAllItems();
 }
--- src/main/java/lk/ijse/posspringbackend/service/ItemServiceImpl.java
+++ src/main/java/lk/ijse/posspringbackend/service/ItemServiceImpl.java
@@ -1,4 +1,68 @@
 package lk.ijse.posspringbackend.service;
 
-public class ItemServiceImpl {
+import lk.ijse.posspringbackend.customObj.ItemResponse;
+import lk.ijse.posspringbackend.dao.ItemDao;
+import lk.ijse.posspringbackend.dto.ItemDTO;
+import lk.ijse.posspringbackend.entity.Item;
+import lk.ijse.posspringbackend.exception.DataPersistsFailedException;
+import lk.ijse.posspringbackend.exception.ItemNotFoundException;
+import lk.ijse.posspringbackend.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ItemServiceImpl implements ItemService{
+    @Autowired
+    private ItemDao itemDao;
+    @Autowired
+    private Mapping mapping;
+
+    @Override
+    public void saveItem(ItemDTO itemDTO) {
+        Item save = itemDao.save(mapping.convertToItem(itemDTO));
+        if (save == null){
+            throw new DataPersistsFailedException("cannot save item");
+        }
+    }
+
+    @Override
+    public void updateItem(String code ,ItemDTO itemDTO) {
+        Optional<Item> tmp = itemDao.findById(code);
+        if (!tmp.isPresent()) {
+            throw new ItemNotFoundException("item not found");
+        }else {
+            tmp.get().setName(itemDTO.getName());
+            tmp.get().setPrice(itemDTO.getPrice());
+            tmp.get().setQty(itemDTO.getQty());
+        }
+    }
+
+    @Override
+    public void deleteItem(String code) {
+        Optional<Item>tmp=itemDao.findById(code);
+        if(!tmp.isPresent()){
+            throw new ItemNotFoundException("Item not found");
+        }else{
+            itemDao.deleteById(code);
+        }
+    }
+
+    @Override
+    public ItemResponse getSelectedItem(String code) {
+        if(itemDao.existsById(code)){
+            return mapping.convertToItemDTO(itemDao.getReferenceById(code));
+        }else{
+            throw new ItemNotFoundException("Item not found ");
+        }
+    }
+
+    @Override
+    public List<ItemDTO> getAllItems() {
+        return mapping.convertItemToList(itemDao.findAll());
+    }
 }
